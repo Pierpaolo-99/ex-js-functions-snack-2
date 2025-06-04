a61aa85a496ac508bde47e404ee3470476aa9551
Pierpaolo-99 <pierpaolodellioio@gmail.com>
2025-06-04 16:14:58 +0200

    Add initial HTML and JavaScript files for timer function implementation

---
+++ Snack_4/main.js
@@ -0,0 +1,13 @@
+/* Crea un generatore di funzioni creaTimer
+Scrivi una funzione creaTimer che accetta un tempo (in ms) e restituisce una nuova funzione che avvia un setTimeout per stampare "Tempo scaduto!". */
+
+function creaTimer(ms) {
+    return function () {
+        setTimeout(() => {
+            console.log("Tempo scaduto!");
+        }, ms);
+    };
+}
+
+const timer3s = creaTimer(3000);
+timer3s();
